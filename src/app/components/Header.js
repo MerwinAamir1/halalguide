1376cf34edf8d4f37efec3f94cf448688ba25cb8
Merwin aamir <merwinaamir1@gmail.com>
2023-12-27 19:02:24 -0500

    added search suggestions using dummy data

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,22 +1,65 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Header() {
+  const dummyData = [
+    "GoodBurger",
+    "Halal Food",
+    "Vegan Delight",
+    "Pizza Palace",
+    "Sushi Corner",
+  ];
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+
+    if (value.length > 0) {
+      const filteredSuggestions = dummyData.filter((item) =>
+        item.toLowerCase().includes(value.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
   return (
     <div className="flex bg-white w-full h-20 p-3 items-center">
       <h1 className="font-extrabold mr-3 ml-3 cursor-pointer">Halal Guide</h1>
 
       <div className="flex flex-auto justify-center">
-        <div className="relative hidden sm:flex flex-grow max-w-screen-lg mx-1">
+        <div className="relative sm:flex flex-grow max-w-screen-lg mx-1">
           <input
-            className="bg-gray-100 rounded-xl p-4 h-11 focus:outline-none w-full  focus:bg-white focus:ring-1 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-black transition duration-300"
+            className="hidden md:flex bg-gray-100 rounded-xl p-4 h-11 focus:outline-none w-full  focus:bg-white focus:ring-1 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-black transition duration-300"
             type="search"
             placeholder="Find Halal Food..."
+            value={searchTerm}
+            onChange={handleChange}
           />
-          <span className="rounded-xl absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+          <span className="hidden md:flex rounded-xl absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
             <MagnifyingGlassIcon className="w-6 h-6 text-gray-500" />
           </span>
+          {suggestions.length > 0 && (
+            <ul className="absolute z-10 bg-white border border-gray-200 rounded-md mt-12 max-h-60 overflow-y-auto w-full">
+              {suggestions.map((item, index) => (
+                <li
+                  key={index}
+                  className="p-3 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => {
+                    setSearchTerm(item);
+                    setSuggestions("");
+                  }}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
 
